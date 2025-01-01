4a97ec1f974473cf7b5ed87b36b6195b97c3373d
Rydhampreet <rydhampreetsingh.gindra@gmail.com>
2025-01-01 14:39:53 +0530

    Changes to cors policy

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ALLOWED_ORIGINS,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
